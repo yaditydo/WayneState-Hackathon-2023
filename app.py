89e6f302741f968f20f29c57485a62f0e01e4ac4
yadito <epardo1742@proton.me>
2023-03-05 04:28:34 -0500

    ccheck

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-from flask import Flask, render_templater
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():        #Home page select track
+    return render_template('index.html')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
